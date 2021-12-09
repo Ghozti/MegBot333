3488d5ccd7bdbb506366b028330071da60874526
ybald <ybalderas1005@gmail.com>
2021-12-08 21:30:11 -0500

    - yay it works

--- src/main/java/ghozti/meg/EventNotifier.java
+++ src/main/java/ghozti/meg/EventNotifier.java
@@ -1,13 +1,25 @@
 package ghozti.meg;
 
+import database.DataBaseMainMain;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.EventListener;
+import java.util.Locale;
 
 public class EventNotifier implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        
+        if (event.getMessageContent().equals("!meg -get events".toLowerCase())){
+            try {
+                for (String i : DataBaseMainMain.getSheetData()){
+                    event.getChannel().sendMessage(i);
+                }
+            } catch (IOException | GeneralSecurityException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/ghozti/meg/Main.java
+++ src/main/java/ghozti/meg/Main.java
@@ -1,11 +1,16 @@
 package ghozti.meg;
 
+import database.DataBaseMainMain;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
         DiscordApi api = new DiscordApiBuilder().setToken(Token.token).login().join();
+        api.addMessageCreateListener(new EventNotifier());
     }
 }
