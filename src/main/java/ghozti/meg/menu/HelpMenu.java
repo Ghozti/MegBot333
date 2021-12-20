520b696d935cccd6162e91f6a0b35d137f46c2b3
Ghozti <ybalderas1005@gmail.com>
2021-12-20 15:35:06 -0500

    - finished help menu

--- src/main/java/ghozti/meg/menu/HelpMenu.java
+++ src/main/java/ghozti/meg/menu/HelpMenu.java
@@ -12,7 +12,13 @@ public class HelpMenu implements MessageCreateListener {
         if (event.getMessageContent().equalsIgnoreCase("!meg -help")){
             EmbedBuilder builder = new EmbedBuilder().setAuthor("Meg 333", "http://google.com/", "https://cdn.discordapp.com/attachments/915970934215688235/921978568051929158/frc333-8.png")
                     .setColor(Color.MAGENTA)
-                    .setTitle("Help menu");
+                    .setTitle("Megalodon Bot Help menu")
+                    .setDescription("The meg is meant to be a personalized discord bot for the frc team, it provides some utility functions such as:")
+                    .addField("Candy Cartel","Search either individual items or the entire list")
+                    .addInlineField("!meg -get cartel list", "gets the entire candy cartel stock data")
+                    .addInlineField("!meg -get cartel item: <candy name>", "will search for the candy specified (note: do not type <>, just type the name. You must also type the name as specified in the list)")
+                    .addField("Event Notifier","will get the month's events (they can be either robotics or JDHS related)")
+                    .addInlineField("!meg -get events","will get the events for the month");
             event.getChannel().sendMessage(builder);
         }
     }
