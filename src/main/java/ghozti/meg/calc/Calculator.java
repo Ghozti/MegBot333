50d01b0fb1325b85c076fa90c3c2b6e1349255ac
Ghozti <ybalderas1005@gmail.com>
2021-12-25 18:52:59 -0500

    - worked on Calculator.java

--- src/main/java/ghozti/meg/calc/Calculator.java
+++ src/main/java/ghozti/meg/calc/Calculator.java
@@ -1,4 +1,60 @@
 package ghozti.meg.calc;
 
 public class Calculator {
+
+    //the calculator utilty class
+
+    private static boolean isListEmpty(float[] nums){return nums.length == 0;}
+
+    public static float add(float[] nums){
+        if (isListEmpty(nums)) return 0;
+        float sum = 0;
+        for (float i : nums) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static float sub(float[] nums){
+        if (isListEmpty(nums)) return 0;
+        float remainder = nums[1];
+        int runs = 1;
+        for (float i : nums) {
+            if (runs > 1){
+                remainder -= i;
+            }
+            runs++;
+        }
+        return remainder;
+    }
+
+    public static float mul(float[] nums){
+        if (isListEmpty(nums)) return 0;
+        float product = nums[0];
+        int runs = 1;
+        for (float i : nums) {
+            if (runs > 1){
+                product *= i;
+            }
+            runs++;
+        }
+        return product;
+    }
+
+    public static float div(float[] nums){
+        if (isListEmpty(nums)) return 0;
+        float quotient = nums[0];
+        int runs = 1;
+        try {
+            for (float i : nums) {
+                if (runs > 1) {
+                    quotient /= i;
+                }
+                runs++;
+            }
+        }catch (Exception e){
+            return 0;
+        }
+        return quotient;
+    }
 }
